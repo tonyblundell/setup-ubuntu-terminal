905bec4a3b227a73ed6006b8446f37b3934dfed0
Tony Blundell <tonyblundell@gmail.com>
2013-04-24 19:34:05 -0400

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 setup-ubuntu-terminal
 =====================
+
+My script for setting up an Ubuntu terminal.
+
+Installs zsh, oh-my-zsh, tmux, vim and git, and slaps on a solarized theme.
+
+Works with Ubuntu 12.04.
---
+++ setup-ubuntu-terminal.sh
@@ -0,0 +1,33 @@
+# Install apps via apt
+sudo apt-get install curl git tmux vim zsh
+
+# Install oh-my-zsh
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+
+# Change default shell to ZSH
+chsh -s /usr/bin/zsh
+
+# Set 256 color mode for ZSH and tmux
+echo "set -g terminal-overrides 'xterm:colors=256'" >> .tmux.conf
+
+# Set solarized colors for terminal
+cd ~
+git clone https://github.com/sigurdga/gnome-terminal-colors-solarized.git
+cd gnome-terminal-colors-solarized
+chmod u+x set_dark.sh
+./set_dark.sh 
+cd ~
+rm -rf gnome-terminal-colors-solarized
+
+# Install vim config
+git clone --recursive https://github.com/tonyblundell/.vim.git
+ln -s .vim/vimrc .vimrc
+
+# Install Inconsolata font
+mkdir ~/.fonts
+cd ~/.fonts
+wget http://www.levien.com/type/myfonts/Inconsolata.otf
+
+# Friendly advice
+echo "Inconsolata font installed, select it in terminal profile settings"
+echo "Log out then log back in for default shell setting to take effect"
